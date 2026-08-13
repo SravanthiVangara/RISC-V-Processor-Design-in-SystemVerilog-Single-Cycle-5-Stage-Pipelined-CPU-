b5133758e74d68475f93266145800d062268f034
SravanthiVangara <sravanthivangara8@gmail.com>
2026-08-13 11:40:09 +0530

    Update hazard_tb.sv

--- tb/hazard_tb.sv
+++ tb/hazard_tb.sv
@@ -1,39 +1,81 @@
-// Code your testbench here
-// or browse Examples
 module hazard_tb;
 
-reg [4:0] id_rs1;
-reg [4:0] id_rs2;
+    logic [4:0] id_rs1;
+    logic [4:0] id_rs2;
 
-reg [4:0] ex_rd;
-reg ex_memread;
+    logic [4:0] ex_rd;
+    logic       ex_memread;
 
-wire stall;
+    logic stall;
 
-hazard_detection dut(
-id_rs1,
-id_rs2,
-ex_rd,
-ex_memread,
-stall
-);
-initial begin
-  $dumpfile("dump.vcd"); 
-  $dumpvars(0,hazard_tb);
-end
-initial begin
+    hazard_detection dut (
+        .id_rs1(id_rs1),
+        .id_rs2(id_rs2),
+        .ex_rd(ex_rd),
+        .ex_memread(ex_memread),
+        .stall(stall)
+    );
 
-id_rs1 = 5'd3;
-id_rs2 = 5'd4;
+    initial begin
 
-ex_rd = 5'd3;
-ex_memread = 1;
+        // --------------------------------
+        // Test 1: Load-use hazard
+        // --------------------------------
+        id_rs1 = 5'd3;
+        id_rs2 = 5'd4;
 
-#10;
+        ex_rd = 5'd3;
+        ex_memread = 1'b1;
 
-$display("STALL = %b",stall);
+        #10;
 
-end
+        $display("TEST 1: Load-use hazard");
+        $display("STALL = %b", stall);
 
+        // --------------------------------
+        // Test 2: No dependency
+        // --------------------------------
+        id_rs1 = 5'd5;
+        id_rs2 = 5'd6;
+
+        ex_rd = 5'd3;
+        ex_memread = 1'b1;
+
+        #10;
+
+        $display("TEST 2: No dependency");
+        $display("STALL = %b", stall);
+
+        // --------------------------------
+        // Test 3: Not a load
+        // --------------------------------
+        id_rs1 = 5'd3;
+        id_rs2 = 5'd4;
+
+        ex_rd = 5'd3;
+        ex_memread = 1'b0;
+
+        #10;
+
+        $display("TEST 3: EX instruction is not a load");
+        $display("STALL = %b", stall);
+
+        // --------------------------------
+        // Test 4: Dependency on x0
+        // --------------------------------
+        id_rs1 = 5'd0;
+        id_rs2 = 5'd4;
+
+        ex_rd = 5'd0;
+        ex_memread = 1'b1;
+
+        #10;
+
+        $display("TEST 4: x0 dependency");
+        $display("STALL = %b", stall);
+
+        $finish;
+
+    end
 
 endmodule
